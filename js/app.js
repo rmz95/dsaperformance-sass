e765e91fc2511f471a7c302e99152c165221ef8d
Luis Ramirez <luisramirez.tapia@hotmail.com>
2022-06-09 20:42:31 -0300

    Proyecto Final JS Coderhouse Parte 2

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
 const contenedorProductos = document.getElementById('contenedor-productos')
 
 let stockProductos =[
-    {   id: 1, tipo: "producto", cantidad: 1,  nombre: "Regulables", precio: 37000},
-    {   id: 2, tipo: "producto", cantidad: 1,  nombre: "Progresivos", precio: 25000},
-    {   id: 3, tipo: "producto", cantidad: 1,  nombre: "1/4 de Milla", precio: 30000},
-    {   id: 4, tipo: "producto", cantidad: 1,  nombre: "Amortiguadores", precio: 33000},
-    {   id: 5, tipo: "producto", cantidad: 1,  nombre: "Espirales", precio: 17000},
-    {   id: 6, tipo: "producto", cantidad: 1,  nombre: "Tren Delantero", precio: 60000},
-    {   id: 7, tipo: "producto", cantidad: 1,  nombre: "Frenos", precio: 40000},
-    {   id: 8, tipo: "servicio", cantidad: 1,  nombre: "Detailing", precio: 10000},
-    {   id: 9, tipo: "servicio", cantidad: 1,  nombre: "Lavado y Limpieza", precio: 6500},
-    {   id: 10, tipo: "servicio", cantidad: 1, nombre: "Tratamiento de Acrilicos", precio: 5000},
-    {   id: 11, tipo: "servicio", cantidad: 1, nombre: "Tratamiento de Espirales y Amortiguadores", precio: 7000},
-    {   id: 12, tipo: "servicio", cantidad: 1, nombre: "Tratamiento de Ceramicos", precio: 8500}
+    {   id: 1, tipo: "Producto", cantidad: 1,  nombre: "Regulables", precio: 37000},
+    {   id: 2, tipo: "Producto", cantidad: 1,  nombre: "Progresivos", precio: 25000},
+    {   id: 3, tipo: "Producto", cantidad: 1,  nombre: "1/4 de Milla", precio: 30000},
+    {   id: 4, tipo: "Producto", cantidad: 1,  nombre: "Amortiguadores", precio: 33000},
+    {   id: 5, tipo: "Producto", cantidad: 1,  nombre: "Espirales", precio: 17000},
+    {   id: 6, tipo: "Producto", cantidad: 1,  nombre: "Tren Delantero", precio: 60000},
+    {   id: 7, tipo: "Producto", cantidad: 1,  nombre: "Frenos", precio: 40000},
+    {   id: 8, tipo: "Servicio", cantidad: 1,  nombre: "Detailing", precio: 10000},
+    {   id: 9, tipo: "Servicio", cantidad: 1,  nombre: "Lavado y Limpieza", precio: 6500},
+    {   id: 10, tipo: "Servicio", cantidad: 1, nombre: "Tratamiento de Acrilicos", precio: 5000},
+    {   id: 11, tipo: "Servicio", cantidad: 1, nombre: "Tratamiento de Espirales y Amortiguadores", precio: 7000},
+    {   id: 12, tipo: "Servicio", cantidad: 1, nombre: "Tratamiento de Ceramicos", precio: 8500}
 ]
 
-//TERCER PASO
-
 const contenedorCarrito = document.getElementById('carrito-contenedor')
-//SEXTO PASO
+
 const botonVaciar = document.getElementById('vaciar-carrito')
-//SEXTIMO PASO, MODIFICAR LOS CONTADORES
+const botonComprar = document.getElementById('btn-buy');
+
 const contadorCarrito = document.getElementById('contadorCarrito')
 
-//OCTAVO PASO
+
 const cantidad = document.getElementById('cantidad')
 const precioTotal = document.getElementById('precioTotal')
 const cantidadTotal = document.getElementById('cantidadTotal')
@@ -36,12 +35,16 @@ document.addEventListener('DOMContentLoaded', () => {
         actualizarCarrito()
     }
 })
-//SEXTO PASO
+
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
     actualizarCarrito()
 })
 
+botonComprar.addEventListener('click', (confirmacionCompra) => {
+    carrito.length = 0
+    actualizarCarrito()
+})
 
 const lista = document.querySelector('#contenedor-productos')
 
@@ -69,61 +72,49 @@ fetch('../data/products.json')
             })
         })
     })
-// 1- PRIMER PASO
 
-//AGREGAR AL CARRITO
+
 const agregarAlCarrito = (prodId) => {
 
-    //PARA AUMENTAR LA CANTIDAD Y QUE NO SE REPITA
-    const existe = carrito.some (prod => prod.id === prodId) //comprobar si el elemento ya existe en el carro
+    //Aumenta la cantidad y no se va a repetir
+    const existe = carrito.some (prod => prod.id === prodId) 
 
-    if (existe){ //SI YA ESTÁ EN EL CARRITO, ACTUALIZAMOS LA CANTIDAD
-        const prod = carrito.map (prod => { //creamos un nuevo arreglo e iteramos sobre cada curso y cuando
-            // map encuentre cual es el q igual al que está agregado, le suma la cantidad
+    if (existe){ 
+        const prod = carrito.map (prod => {
             if (prod.id === prodId){
                 prod.cantidad++
             }
         })
-    } else { //EN CASO DE QUE NO ESTÉ, AGREGAMOS EL CURSO AL CARRITO
-        const item = stockProductos.find((prod) => prod.id === prodId)//Trabajamos con las ID
-        //Una vez obtenida la ID, lo que haremos es hacerle un push para agregarlo al carrito
+    } else { 
+        const item = stockProductos.find((prod) => prod.id === prodId)
         carrito.push(item)
     }
-    //Va a buscar el item, agregarlo al carrito y llama a la funcion actualizarCarrito, que recorre
-    //el carrito y se ve.
-    actualizarCarrito() //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
-    //MODIFICA EL CARRITO
-}
-//agregarAlCarrito(1) //Le pasamos el ID por parametro. Tenemos que asigarle como evento esta funcion al boton
-//con el id de su producto correspondiente
 
-// 5 - QUINTO PASO
+    actualizarCarrito()
+}
+
+
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId)
 
-    const indice = carrito.indexOf(item) //Busca el elemento q yo le pase y nos devuelve su indice.
+    const indice = carrito.indexOf(item)
 
-    carrito.splice(indice, 1) //Le pasamos el indice de mi elemento ITEM y borramos 
-    // un elemento 
-    actualizarCarrito() //LLAMAMOS A LA FUNCION QUE CREAMOS EN EL TERCER PASO. CADA VEZ Q SE 
-    //MODIFICA EL CARRITO
+    carrito.splice(indice, 1) 
+    
+    actualizarCarrito()
     console.log(carrito)
 }
 
 const actualizarCarrito = () => {
-    //4- CUARTO PASO
-    //LOS APPENDS SE VAN ACUMULANDO CON LO QE HABIA ANTES
-    contenedorCarrito.innerHTML = "" //Cada vez que yo llame a actualizarCarrito, lo primero q hago
-    //es borrar el nodo. Y despues recorro el array lo actualizo de nuevo y lo rellena con la info
-    //actualizado
-    //3 - TERCER PASO. AGREGAR AL MODAL. Recorremos sobre el array de carrito.
+    
+    contenedorCarrito.innerHTML = ""
 
-    //Por cada producto creamos un div con esta estructura y le hacemos un append al contenedorCarrito (el modal)
     carrito.forEach((prod) => {
         const div = document.createElement('div')
         div.className = ("productoEnCarrito")
         div.innerHTML = `
         <p class="my-0">${prod.nombre}</p>
+        <p class="my-0">${prod.tipo}</p>
         <p class="my-0">Precio:$${prod.precio}</p>
         <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
         <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar btn btn-secondary my-2">Eliminar</button>
@@ -134,36 +125,37 @@ const actualizarCarrito = () => {
         localStorage.setItem('carrito', JSON.stringify(carrito))
 
     })
-    //SEPTIMO PASO
-    contadorCarrito.innerText = carrito.length // actualizamos con la longitud del carrito.
-    //OCTAVO PASO
+    contadorCarrito.innerText = carrito.length
     console.log(carrito)
     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
-    //Por cada producto q recorro en mi carrito, al acumulador le suma la propiedad precio, con el acumulador
-    //empezando en 0.
 
 }
 
-let compra2 = document.querySelector("#btnbuy")
-compra2.addEventListener("click", respuestaBuy);{
-    function respuestaBuy() {
-        Swal.fire({
-            title: '¿Estas seguro de comprar estos servicios?',
-            text: "Si no esta seguro, click en Cancelar",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Si, estoy seguro.'
+// function comprarBotonClicked(){
+//     contenedorCarrito.innerHTML = '';
+//     actualizarCarrito();
+// }
 
-            }).then((result) => {
+// let compra2 = document.querySelector("#btnbuy")
+// compra2.addEventListener("click", respuestaBuy);{
+//     function respuestaBuy() {
+//         Swal.fire({
+//             title: '¿Estas seguro de comprar estos servicios?',
+//             text: "Si no esta seguro, click en Cancelar",
+//             icon: 'warning',
+//             showCancelButton: true,
+//             confirmButtonColor: '#3085d6',
+//             cancelButtonColor: '#d33',
+//             confirmButtonText: 'Si, estoy seguro.'
+
+//             }).then((result) => {
             
-            if (result.isConfirmed) {
-                Swal.fire(
-                'Hecho!',
-                'Tu compra ha sido procesada',
-                'success'
-                )}
-        })
-    }
-}
+//             if (result.isConfirmed) {
+//                 Swal.fire(
+//                 'Hecho!',
+//                 'Tu compra ha sido procesada',
+//                 'success'
+//                 )}
+//         })
+//     }
+// }
